68ed252801229145d4571a96e815540bde8e4da5
jetblack <coolanimeguy666@gmail.com>
2023-07-26 15:48:48 -0600

    Fixed some gross code

--- FumitosLexer.py
+++ FumitosLexer.py
@@ -34,13 +34,19 @@ class ParseIntoAssembly:
     def ParserMain(self, File:typing.TextIO, outputPath)->typing.TextIO:
         #return output file...
         jSettings = json.load(open('InitSettings.json'))
+        print(jSettings)
         if 'asm' not in outputPath:
             outputPath+ '.asm'
         outFile = open(outputPath, 'w')
         outFile.write(jSettings['initMain'])
-
+        #improve the lexing/reading 
         for line in File.readlines():
-            [self.operationsDict[char](outFile) for x in range(len(line)) for char in self.operationsDict if line[x: x+len(char)] == char]
+            #some unhinged code ... why did i write this? why does it "work"?
+            #i was something else durring hs
+            for x in range(len(line)):
+                if line[x] in self.operationsDict:
+                    self.operationsDict[line[x]](outFile)
+            # [self.operationsDict[char](outFile)  for char in self.operationsDict if line[x] == char]
 
         print('finished writing')
         outFile.write(jSettings['endMain'])
